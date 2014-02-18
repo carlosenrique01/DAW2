73ceab3f31fa0637bd1415120403e01f2b9a8f25
carlos <carlos360@live.com.mx>
2014-02-18 12:36:17 -0600

    prac 0

---
+++ Pk0_EmptyWeb/Pk0_EmptyWeb/Controllers/homeController.vb
@@ -0,0 +1,13 @@
+﻿Namespace Pk0_EmptyWeb
+    Public Class homeController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /home
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
---
+++ Pk0_EmptyWeb/Pk0_EmptyWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Pk0_EmptyWeb.MvcApplication" Language="VB" %>
